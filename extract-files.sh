SyberiaProject-Devices/platform_device_xiaomi_mido

mido: uncommonize tree
* Why use a common vendor for uncommon device tree...

extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,52 +23,61 @@ VENDOR=xiaomi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-SYBERIA_ROOT="$MY_DIR"/../../..
+SYBERIA_ROOT="${MY_DIR}/../../.."
 
-HELPER="$SYBERIA_ROOT"/vendor/syberia/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="$SYBERIA_ROOT/vendor/syberia/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC=$1
-                                ;;
+SECTION=
+KANG=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$SYBERIA_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${SYBERIA_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
-extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
+extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
+            "${KANG}" --section "${SECTION}"
 
-DEVICE_BLOB_ROOT="$SYBERIA_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+DEVICE_BLOB_ROOT="${SYBERIA_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
+    
+sed -i \
+	's/\/system\/etc\//\/vendor\/etc\//g' \
+	"${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera2_sensor_modules.so"
 
 sed -i \
-    's/\/system\/etc\//\/vendor\/etc\//g' \
-    "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
+	"s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
+	"${DEVICE_BLOB_ROOT}/vendor/bin/mm-qcamera-daemon"
 
-sed -i \
-     "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
-     "$DEVICE_BLOB_ROOT"vendor/bin/mm-qcamera-daemon
+"${MY_DIR}/setup-makefiles.sh" "${CLEAN_VENDOR}"
 
-"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,51 +18,35 @@
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2017
-
 # Required!
 export DEVICE=mido
-export DEVICE_COMMON=msm8953-common
 export VENDOR=xiaomi
-
 export DEVICE_BRINGUP_YEAR=2017
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-SYBERIA_ROOT="$MY_DIR"/../../..
+SYBERIA_ROOT="${MY_DIR}/../../.."
 
-HELPER="$SYBERIA_ROOT"/vendor/syberia/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="$SYBERIA_ROOT/vendor/syberia/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
+
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$SYBERIA_ROOT" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${SYBERIA_ROOT}" false
 
 # Copyright headers and guards
-write_headers "mido tissot"
+write_headers
 
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
+# The standard device blobs
+write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
-# We are done!
+# Finish
 write_footers
 
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$SYBERIA_ROOT" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
-
-    # We are done!
-    write_footers
-fi

sayan7848/device_xiaomi_mido

Signed-off-by: sayan7848 <sayan7848@gmail.com>

extract-files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,63 +19,65 @@
 set -e
 
 DEVICE=mido
-DEVICE_COMMON=msm8953-common
 VENDOR=xiaomi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-DU_ROOT="$MY_DIR"/../../..
+DU_ROOT="${MY_DIR}/../../.."
 
-HELPER="$DU_ROOT"/vendor/du/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="$DU_ROOT/vendor/du/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC=$1
-                                ;;
+SECTION=
+KANG=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$DU_ROOT" true "$CLEAN_VENDOR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${DU_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files-qc.txt "$SRC" "$SECTION"
+extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
+            "${KANG}" --section "${SECTION}"
 
-if [ -s "$MY_DIR"/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$DU_ROOT" false "$CLEAN_VENDOR"
+DEVICE_BLOB_ROOT="${DU_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
+    
+sed -i \
+	's/\/system\/etc\//\/vendor\/etc\//g' \
+	"${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera2_sensor_modules.so"
 
-    extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+sed -i \
+	"s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
+	"${DEVICE_BLOB_ROOT}/vendor/bin/mm-qcamera-daemon"
 
-    DEVICE_BLOB_ROOT="$DU_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+"${MY_DIR}/setup-makefiles.sh" "${CLEAN_VENDOR}"
 
-    sed -i \
-        's/\/system\/etc\//\/vendor\/etc\//g' \
-        "$DEVICE_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_modules.so
-
-    sed -i \
-         "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" \
-         "$DEVICE_BLOB_ROOT"/vendor/bin/mm-qcamera-daemon
-fi
-
-"$MY_DIR"/setup-makefiles.sh

setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,51 +18,35 @@
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2017
-
 # Required!
 export DEVICE=mido
-export DEVICE_COMMON=msm8953-common
 export VENDOR=xiaomi
-
 export DEVICE_BRINGUP_YEAR=2017
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-DU_ROOT="$MY_DIR"/../../..
+DU_ROOT="${MY_DIR}/../../.."
 
-HELPER="$DU_ROOT"/vendor/du/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="$DU_ROOT/vendor/du/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
+
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$DU_ROOT" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${DU_ROOT}" false
 
 # Copyright headers and guards
-write_headers "mido tissot"
+write_headers
 
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
+# The standard device blobs
+write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
-# We are done!
+# Finish
 write_footers
 
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$DU_ROOT" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
-
-    # We are done!
-    write_footers
-fi