425cc9998c964c4e00f1ab482709ebc4d30395fc
sayan7848 <sayan7848@gmail.com>
2019-06-21 18:22:35 +0000

    Revert "mido: Add basic USB HAL that reports no status change"
    This reverts commit b62d7d6593087e9aa7490c4ab66c7e87d3aa9fa5.
    
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- device.mk
+++ device.mk
@@ -343,7 +343,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.xiaomi_mido
+    android.hardware.usb@1.0-service.basic
 
 # Vibrator
 PRODUCT_PACKAGES += \
